e670de63ed18ad95b3a675ffee6878c4e9fc9717
Boyko Anton <boyko.ant@live.com>
2016-12-26 20:31:32 +0200

    Common Configuration Readers

---
+++ Readers/ZumoCommunity.ConfigurationAPI.Readers.Common/AppSettingsReader.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using ZumoCommunity.ConfigurationAPI.Infrastructure.Services;
+
+namespace ZumoCommunity.ConfigurationAPI.Readers.Common
+{
+	public sealed class AppSettingsReader : IConfigurationReader
+	{
+		public async Task<string> GetConfigValueAsync(string key)
+		{
+			return ConfigurationManager.AppSettings.Get(key);
+		}
+	}
+}
---
+++ Readers/ZumoCommunity.ConfigurationAPI.Readers.Common/EnvironmentVariablesReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using ZumoCommunity.ConfigurationAPI.Infrastructure.Services;
+
+namespace ZumoCommunity.ConfigurationAPI.Readers.Common
+{
+	public sealed class EnvironmentVariablesReader : IConfigurationReader, IConfigurationWriter
+	{
+		public async Task<string> GetConfigValueAsync(string key)
+		{
+			return Environment.GetEnvironmentVariable(key);
+		}
+
+		public async Task SetConfigValueAsync(string key, string value)
+		{
+			Environment.SetEnvironmentVariable(key, value);
+		}
+	}
+}
