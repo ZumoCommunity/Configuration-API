867a73d1b93e315513bbf8415f6eb29bce2e135e
Boyko Anton <boyko.ant@live.com>
2016-12-26 20:30:43 +0200

    Infrastructure

---
+++ ZumoCommunity.ConfigurationAPI.Infrastructure/Services/IConfigurationWriter.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ZumoCommunity.ConfigurationAPI.Infrastructure.Services
+{
+	public interface IConfigurationWriter
+	{
+		Task SetConfigValueAsync(string key, string value);
+	}
+}
